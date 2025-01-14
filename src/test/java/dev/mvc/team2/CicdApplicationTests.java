903102783dd1ddf6a507515f84560e832d43af52
kdc3246 <baba001109@naver.com>
2025-01-14 10:11:43 +0900

    첫번째 커밋

---
+++ src/test/java/dev/mvc/team2/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mvc.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
